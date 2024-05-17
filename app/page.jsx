92fbee2c1249c431343c3134345bb5d168489813
davidblockify <david.nguyen@blockify.it>
2024-05-17 10:32:04 +0700

    add background video

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,6 @@
 'use client'
 import Image from 'next/image'
-import logo from './asset/company-logo.png'
+import logo from './asset/logo-banner.png'
 import gsap from 'gsap'
 import { useGSAP } from '@gsap/react'
 import NavBar from './components/Navbar'
@@ -23,23 +23,27 @@ export default function Home() {
         snap: {
           snapTo: 'labels',
           ease: 'circ.in',
-
-        }
+        },
       },
     })
     gsap.from(container.current, {
       opacity: 0,
+      scale: 0.3,
       duration: 3,
       ease: 'circ.out',
     })
     tl.to(container.current, {
       opacity: 0,
       y: 400,
+      scale: 0,
     })
   }, [])
 
   return (
-    <div className="bg-landing-page h-screen bg-fixed bg-center bg-no-repeat bg-cover">
+    <div className="w-full h-screen overflow-x-hidden ">
+      <video autoPlay loop muted className="fixed top-0 left-0 min-w-[100%] min-h-[100%]a -z-10">
+        <source src="/background-banner.mp4" type="video/mp4" />
+      </video>
       <NavBar />
       <div className="w-full h-full flex flex-col items-center justify-center">
         <Image
@@ -47,8 +51,8 @@ export default function Home() {
           id="logo"
           className="logo"
           src={logo}
-          width={500}
-          height={500}
+          width={700}
+          height={700}
           alt="logo"
         />
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,8 @@ module.exports = {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-        "landing-page": "url('../app/asset/landing-page-bg.jpg')"
+        // "landing-page": "url('../app/asset/landing-page-bg.jpg')"
+        "landing-page": "url('/background-banner.mp4')"
       },
     },
   },
