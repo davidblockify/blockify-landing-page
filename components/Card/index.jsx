bc664b1b58c8ede14f07c10c2c60d77779e93f7e
bee-nguyen <bee.nguyen@blockify.it>
2024-05-22 15:59:36 +0700

    feat: fix eror prettier

--- components/Card/index.jsx
+++ components/Card/index.jsx
@@ -3,27 +3,27 @@ import Image from 'next/image'
 import { CardContent, Typography } from '@mui/material'
 
 export default function Card({ imgSrc, title, description }) {
-    return (
-        <CardContent className="bg-white relative">
-            {imgSrc && (
-                <Image
-                    src={imgSrc}
-                    width={100}
-                    height={100}
-                    alt={title}
-                    loading="lazy"
-                    className="absolute -top-12 left-5"
-                />
-            )}
-            <Typography
-                fontWeight="bold"
-                gutterBottom
-                variant="h5"
-                className={`${imgSrc ? 'pt-10' : ''}`}
-            >
-                {title}
-            </Typography>
-            <Typography variant="body2">{description}</Typography>
-        </CardContent>
-    )
+  return (
+    <CardContent className="bg-white relative">
+      {imgSrc && (
+        <Image
+          src={imgSrc}
+          width={100}
+          height={100}
+          alt={title}
+          loading="lazy"
+          className="absolute -top-12 left-5"
+        />
+      )}
+      <Typography
+        fontWeight="bold"
+        gutterBottom
+        variant="h5"
+        className={`${imgSrc ? 'pt-10' : ''}`}
+      >
+        {title}
+      </Typography>
+      <Typography variant="body2">{description}</Typography>
+    </CardContent>
+  )
 }
