d061242431eceed11465e9f33e4eb94ad99f8832
rrenae <renae.nguyen@blockify.it>
2024-05-22 13:39:44 +0700

    fix images styling

--- components/AboutUs/Frame4/index.jsx
+++ components/AboutUs/Frame4/index.jsx
@@ -11,7 +11,7 @@ import image3 from '@/public/about-us-4-3.png'
 import { GREEN_TEXT_COLOR } from '@/shared/constants'
 
 export default function Frame4() {
-  const frame4Header = (
+  const frame4Headline = (
     <>
       <span style={{ fontWeight: 'bold', color: GREEN_TEXT_COLOR }}>
         Engagement
@@ -22,7 +22,7 @@ export default function Frame4() {
       </span>
     </>
   )
-  const frame4Content = (
+  const frame4BodyText = (
     <span>
       Join us at Blockify to explore how blockchain can revolutionize your
       operations and connect you to the future of a fully integrated, global
@@ -46,27 +46,38 @@ export default function Frame4() {
       style={{ width: '100%', height: 'auto' }}
     />
   )
-  const Image3 = <Image src={image3} alt="image-4-3" height={428} width={291} />
+  const Image3 = (
+    <Image
+      src={image3}
+      alt="image-4-3"
+      sizes="100vw"
+      style={{ width: '100%', height: 'auto' }}
+    />
+  )
   return (
     <Box m="10%" width="80vw" display="flex" flexDirection="row">
       <Box
         mr={{ xs: 0, md: 2.5 }}
         flexDirection="column"
         justifyItems="center"
-        sx={{ width: { xs: '100%', md: '50%', lg: '50%' } }}
+        sx={{ width: { xs: '100%', md: '50%' } }}
       >
         <VerticalBanner
-          headline={frame4Header}
-          bodyText={frame4Content}
+          headline={frame4Headline}
+          bodyText={frame4BodyText}
           media={Image1}
           textAlign={{ xs: 'center', md: 'left' }}
           alignItems={{ xs: 'center', md: 'flex-start' }}
         />
       </Box>
-      <Box display={{ xs: 'none', md: 'flex' }} flexDirection="column">
+      <Box
+        position="relative"
+        display={{ xs: 'none', md: 'flex' }}
+        flexDirection="column"
+        width="50%"
+      >
         <Box mb={4}> {Image2}</Box>
-
-        {Image3}
+        <Box width="66%">{Image3}</Box>
       </Box>
     </Box>
   )
