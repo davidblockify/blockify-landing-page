6aaae16d372bab5737d69ec0383153eb9e67d5bf
bee-nguyen <bee.nguyen@blockify.it>
2024-05-23 15:25:16 +0700

    feat: update banner image

---
+++ components/HeadingText/index.jsx
@@ -0,0 +1,10 @@
+import Typography from '@mui/material/Typography'
+
+export default function HeadingText({ isUnderLine, title }) {
+  return (
+    <div className="flex flex-col items-center mb-20">
+      <Typography variant="h2">{title}</Typography>
+      {isUnderLine && <hr className="w-44 border-black rounded-sm" />}
+    </div>
+  )
+}
--- components/ViewportBackground/index.jsx
+++ components/ViewportBackground/index.jsx
@@ -2,13 +2,19 @@
 
 import { usePathname } from 'next/navigation'
 
+import Background from '../Background'
+
 export default function ViewportBackground({ children }) {
   const pathname = usePathname()
-
-  const homeStyles =
-    pathname === '/' ? 'bg-[url(/background.png)] bg-cover bg-center' : ''
+  const isHome = pathname === '/'
+  const isServicePage = pathname === '/service'
 
   return (
-    <div className={`mx-auto my-10 h-screen ${homeStyles}`}>{children}</div>
+    <div
+      className={`mx-auto my-16 h-screen ${isHome ? 'bg-[url(/background.png)] bg-cover bg-center' : ''}`}
+    >
+      {isServicePage && <Background imgSrc="service-banner.png" />}
+      {children}
+    </div>
   )
 }
