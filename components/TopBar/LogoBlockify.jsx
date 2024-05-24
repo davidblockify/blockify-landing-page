d2c4a3ebc2f485b1f11bed16d24d5dd16993aab5
bee-nguyen <bee.nguyen@blockify.it>
2024-05-24 18:24:41 +0700

    feat: change logo

--- components/TopBar/LogoBlockify.jsx
+++ components/TopBar/LogoBlockify.jsx
@@ -2,38 +2,14 @@ import React from 'react'
 
 import Image from 'next/image'
 
-import Typography from '@mui/material/Typography'
 import Box from '@mui/material/Box'
 
-import Logo from '@/public/company-logo.png'
+import Logo from '@/public/logo-with-text.png'
 
 export default function LogoBlockify({ displaySettings }) {
-  const blockifyVariant = displaySettings.md !== 'flex' ? 'h5' : 'h4'
-  const blockifyFlexGrow = displaySettings.md === 'none' ? 1 : undefined
-  const logoSize = displaySettings.md !== 'flex' ? 45 : 60
-
   return (
-    <>
-      <Box sx={{ display: displaySettings, mr: '0.5em' }}>
-        <Image width={logoSize} height="auto" src={Logo} alt="Brand Logo" />
-      </Box>
-
-      <Typography
-        variant={blockifyVariant}
-        color="#01161B"
-        noWrap
-        component="a"
-        href="/"
-        sx={{
-          display: displaySettings,
-          flexGrow: { blockifyFlexGrow },
-          fontFamily: 'monospace',
-          fontWeight: { xs: 400, sm: 500, md: 700 },
-          textDecoration: 'none'
-        }}
-      >
-        Blockify
-      </Typography>
-    </>
+    <Box sx={{ display: displaySettings, mr: '0.5em' }}>
+      <Image width="auto" height="auto" src={Logo} alt="Brand Logo" />
+    </Box>
   )
 }
