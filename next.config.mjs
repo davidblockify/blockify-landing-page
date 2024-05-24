72e13ed5c52d8d518f369a93b9e92037b5aa62c0
davidblockify <david.nguyen@blockify.it>
2024-05-24 18:15:13 +0700

    update nextconfig

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ['mui-tel-input'],
+  trailingSlash: true,
   output: 'export',
   images: {
     unoptimized: true,
